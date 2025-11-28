faf6069d522a71dcba486c755a9a4fc69347930e
Phạm Khánh Đăng <khanhdangfc2004@gmail.com>
2025-11-28 11:31:58 +0700

    Update frontend code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TrangChuComponent } from '../Home/TrangChu/trang-chu-component/trang-chu-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TrangChuComponent },
+
+ 
+];
